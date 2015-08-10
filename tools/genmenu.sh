a3c6786c02bba3c91a5d57a5bad543f8267c0226
Mike D. Lowis <mike.lowis@gentex.com>
2015-08-10 15:25:22 -0400

    Added code to highlight the current

--- tools/genmenu.sh
+++ tools/genmenu.sh
@@ -4,8 +4,8 @@ pushd pages/ > /dev/null
 pagelist=(`grep MENUID *.md | sed -e's/\.md:MENUID//' | awk '{print $2, $1}' | sort | cut -d' ' -f2`)
 
 echo '<span class="left">'
-for page in "${pagelist[@]}"; do
-    echo "            <a class=\"\" href=\"$page.html\">$page</a>"
+for i in "${!pagelist[@]}"; do
+    echo "            <a id=\"menuitem$i\" href=\"${pagelist[$i]}.html\">${pagelist[$i]}</a>"
 done
 echo '        </span>'
 popd > /dev/null
