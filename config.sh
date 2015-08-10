4330fb78d626e1bc694f7139965baeef43ef9713
Mike D. Lowis <mike.lowis@gentex.com>
2015-08-10 14:24:57 -0400

    Fixed truncated subtitle/title

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export TITLE="Simplicity is the ultimate sophistication"
-export SUBTITLE=$TITLE
+export SUBTITLE="$TITLE"
 export COPYRIGHT="© 2015 Michael D. Lowis"
 export STYLE="/site/style.css"
 export LOGO="/site/logo.png"
